791ea9e16433f0114b647be4a325763e7c5d2290
David Betz <agapeton@gmail.com>
2018-02-19 00:00:00 -0600

    add healthchecking

--- app.go
+++ app.go
@@ -32,6 +32,9 @@ func call(address string, ch chan<- string, errChannel chan<- error) {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     var id = strings.Split(r.Host, ":")[0]
+    if (id == "localhost") {
+        id = os.Getenv("NAME")
+    }
     var called = []rune(id)[0]
     errChannel := make(chan error, 1)
     log.Println("+CALLED id:", id, "|called:", called)
@@ -57,12 +60,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func check(letter string) {
+    response, err := http.Get("http://" + letter + ":" + port)
+    if err != nil {
+        panic(err)
+    } else {
+        contents, _ := ioutil.ReadAll(response.Body)
+        log.Println(fmt.Sprintf("%s", contents))
+    }
+}
+
 func main() {
+    var arg string
+    if(len(os.Args) > 1) {
+        arg = os.Args[1]
+    }
     if len(port) == 0 {
         port = "3000"
     }
-
-    http.HandleFunc("/", handler)
-    log.Println("Starting server on port", port)
-    log.Fatal(http.ListenAndServe(":"+port, nil))
+    switch arg {
+        case "check":
+            if(len(os.Args) < 3 || len(os.Args[2]) != 1) {
+                panic("letter required")
+            }
+            check(os.Args[2])
+        default:
+            http.HandleFunc("/", handler)
+            log.Println("Starting server on port", port)
+            log.Fatal(http.ListenAndServe(":"+port, nil))
+    }
 }
