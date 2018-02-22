1a991325289dfab602d8d71ea5d354daa7f895e3
David Betz <agapeton@gmail.com>
2018-02-21 19:08:11 -0600

    cleanup

--- app.go
+++ app.go
@@ -19,12 +19,11 @@ func call(address string, ch chan<- string, errChannel chan<- error) {
     if err != nil {
         errChannel <- errors.New("500")
     } else {
-        contents, _ := ioutil.ReadAll(response.Body)
-        result := fmt.Sprintf("%s", contents)
+        contents, err := ioutil.ReadAll(response.Body)
         if response.StatusCode == 200 {
-            ch <- result
+            ch <- fmt.Sprintf("%s", contents)
         } else {
-            errChannel <- errors.New(result)
+            errChannel <- err
         }
         defer response.Body.Close()
     }
@@ -32,9 +31,6 @@ func call(address string, ch chan<- string, errChannel chan<- error) {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     var id = strings.Split(r.Host, ":")[0]
-    if (id == "localhost") {
-        id = os.Getenv("NAME")
-    }
     var called = []rune(id)[0]
     errChannel := make(chan error, 1)
     log.Println("+CALLED id:", id, "|called:", called)
@@ -46,17 +42,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
             ch <- "$"
         }()
     }
-    ret := func(v string) {
-        v += "," + id
-        log.Println("-RETURNING " + v)
-        w.Write([]byte(v))
-    }
     select {
-    case value := <-ch:
-        ret(value)
-    case err := <-errChannel:
-        w.WriteHeader(500)
-        ret(fmt.Sprintf("%s", err))
+        case value := <-ch:
+            value += "," + id
+            log.Println("-RETURNING " + value)
+            w.Write([]byte(value))
+        case <-errChannel:
+            w.WriteHeader(http.StatusInternalServerError)
     }
 }
 
@@ -79,14 +71,14 @@ func main() {
         port = "3000"
     }
     switch arg {
-        case "check":
-            if(len(os.Args) < 3 || len(os.Args[2]) != 1) {
-                panic("letter required")
-            }
-            check(os.Args[2])
-        default:
-            http.HandleFunc("/", handler)
-            log.Println("Starting server on port", port)
-            log.Fatal(http.ListenAndServe(":"+port, nil))
+    case "check":
+        if(len(os.Args) < 3 || len(os.Args[2]) != 1) {
+            panic("letter required")
+        }
+        check(os.Args[2])
+    default:
+        http.HandleFunc("/", handler)
+        log.Println("Starting server on port", port)
+        log.Fatal(http.ListenAndServe(":"+port, nil))
     }
 }
